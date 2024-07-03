e0fd4dcf785fc41564268d607ada1029a14a5d73
ravigoldmeal <ravi06.goldmedal@gmail.com>
2024-07-03 18:10:32 +0530

    Add project files.

---
+++ Data/BloggiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bloggi.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggi.Data
+{
+    public class BloggiDbContext : DbContext
+    {
+        public BloggiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
